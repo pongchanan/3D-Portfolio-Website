6893d6af18b046356dffd6fb17a7053899dcce02
Pongchanan <pongchanansriwanna2547@gmail.com>
2024-12-04 22:23:02 +0700

    Enhance navbar and toggle link button components with dynamic links and styles

--- frontend/src/app/component/toggle-link-button/toggle-link-button.component.ts
+++ frontend/src/app/component/toggle-link-button/toggle-link-button.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-toggle-link-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toggle-link-button.component.html',
-  styleUrl: './toggle-link-button.component.css'
+  styleUrls: ['./toggle-link-button.component.css'],
 })
 export class ToggleLinkButtonComponent {
+  @Input() buttonColor: 'black' | 'not-show' = 'black';
+  @Input() linkPath: string = '';
+  @Input() buttonText: string = 'Click me';
 
+  onButtonClick() {
+    if (this.linkPath) {
+      window.location.href = this.linkPath;
+    }
+  }
 }
