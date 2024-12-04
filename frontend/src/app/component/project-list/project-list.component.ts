1d35de4d725443cadd40c16f9a1318243377deb0
Pongchanan <pongchanansriwanna2547@gmail.com>
2024-12-04 23:14:34 +0700

    Implement pagination for project list and enhance card styling

--- frontend/src/app/component/project-list/project-list.component.ts
+++ frontend/src/app/component/project-list/project-list.component.ts
@@ -11,4 +11,36 @@ import { CommonModule } from '@angular/common';
 })
 export class ProjectListComponent {
   @Input() projectsArray!: Array<MinProjectType>;
+  currentPage: number = 1;
+  projectsPerPage: number = 3;
+
+  get paginatedProjects() {
+    const startIndex = (this.currentPage - 1) * this.projectsPerPage;
+    return this.projectsArray.slice(
+      startIndex,
+      startIndex + this.projectsPerPage
+    );
+  }
+
+  get totalPages() {
+    return Math.ceil(this.projectsArray.length / this.projectsPerPage);
+  }
+
+  setPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage * this.projectsPerPage < this.projectsArray.length) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
 }
