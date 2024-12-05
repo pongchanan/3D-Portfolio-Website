b91dad1c7f788b691021208a1564bd4961038c31
Pongchanan <pongchanansriwanna2547@gmail.com>
2024-12-05 14:31:26 +0700

    Add responsive behavior to landing page; conditionally render avatar based on screen width

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -3,20 +3,30 @@ import { MoveAvatarComponent } from '../component/move-avatar/move-avatar.compon
 import { ProjectListComponent } from '../component/project-list/project-list.component';
 import { FetchProjectThumbnailService } from '../service/fetch-project-thumbnail.service';
 import { MinProjectType } from '../model/minProject.type';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [MoveAvatarComponent, ProjectListComponent],
+  imports: [MoveAvatarComponent, ProjectListComponent, CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent implements OnInit {
   fetchThumbnailService = inject(FetchProjectThumbnailService);
   projectThumbnail = signal<Array<MinProjectType>>([]);
+  isWideScreen = signal<boolean>(true);
 
   ngOnInit(): void {
+    this.checkScreenWidth();
     this.fetchThumbnailService.fetchProjectThumbnail().subscribe((data) => {
       this.projectThumbnail.set(data);
+    });
+  }
+
+  checkScreenWidth(): void {
+    this.isWideScreen.set(window.innerWidth >= 769);
+    window.addEventListener('resize', () => {
+      this.isWideScreen.set(window.innerWidth >= 769);
     });
   }
 }
