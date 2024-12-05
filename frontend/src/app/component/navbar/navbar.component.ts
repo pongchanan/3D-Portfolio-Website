9f074500d65077c195ac077c3320b5069052bf95
Pongchanan <pongchanansriwanna2547@gmail.com>
2024-12-05 16:01:22 +0700

    Remove unused button components and associated styles for cleaner codebase

--- frontend/src/app/component/navbar/navbar.component.ts
+++ frontend/src/app/component/navbar/navbar.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { PageLinkButtonBlackComponent } from '../page-link-button-black/page-link-button-black.component';
-import { PageLinkButtonWhiteComponent } from '../page-link-button-white/page-link-button-white.component';
-import { WebsiteLinkButtonWhiteComponent } from '../website-link-button-white/website-link-button-white.component';
-import { WebsiteNavButtonWhiteComponent } from '../website-nav-button-white/website-nav-button-white.component';
 import { LinkButtonComponent } from '../link-button/link-button.component';
 
 @Component({
